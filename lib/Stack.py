66aa356282252c179ebf11cf3f02958b307caa50
Chris Lozzi <clozzi12@gmail.com>
2023-12-13 15:54:09 -0500

    all tests passing

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.limit = limit
+        if len(items) <= limit:
+            self.items = items
+
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if (target not in self.items):
+            return -1
+        else:
+            index = self.items.index(target)
+            distance = len(self.items) - index
+            return distance - 1
